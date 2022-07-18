ca26c16f4e3ebc5235c022f4f3a4e34a47492f24
Vlad Kel <108391008+dolsev@users.noreply.github.com>
2022-07-18 14:00:43 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i=0;i<numberOfDrumButtons;i++) {
+    document.querySelectorAll(".drum")[i].addEventListener(("click"), function(){
+    makeSound(this.innerHTML);
+    })
+};
+
+document.addEventListener("keydown", function(event)
+{makeSound(event.key);
+});
+
+function makeSound(key) {
+     switch (key){
+    case "w":
+        var boy=new Audio("sounds/boy-next-door.mp3");
+        boy.play();
+        break;
+    case "a":
+        var fuckyou=new Audio("sounds/fuckyou_N4ocxxs.mp3"); 
+        fuckyou.play();
+        break;
+    case "s":
+        var orgasm=new Audio("sounds/orgasm-6.mp3");
+        orgasm.play();
+        break;
+    case "d":
+        var spank=new Audio("sounds/spank-3.mp3");
+        spank.play();
+        break;
+    case "j":
+        var suction=new Audio("sounds/suction_lXUaIEQ.mp3");
+        suction.play();
+        break;
+    case "k":
+        var thats=new Audio("sounds/thats-amazing_i8ctuCt.mp3");
+        thats.play();
+        break;
+    case "l":
+        var woo=new Audio("sounds/woo_htcxajK.mp3");
+        woo.play();
+        break;
+    }
+}
